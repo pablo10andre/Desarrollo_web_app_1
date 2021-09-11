8fca80691eb48f8f73342cafe2d276b69e700c56
pablo10andre <pablo10andre@gmail.com>
2021-09-11 10:08:25 -0600

    Proveedores_PDF_EXCEL_1

---
+++ desarrollo-web/desarrollo-web/src/main/java/com/desarrolloweb/spring/app/view/excel/ProveedorExcelView.java
@@ -0,0 +1,67 @@
+package com.desarrolloweb.spring.app.view.excel;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractXlsxView;
+
+import com.desarrolloweb.spring.app.entities.Proveedor;
+
+@Component("detalle-proveedor-form.xlsx")
+public class ProveedorExcelView extends AbstractXlsxView {
+
+	@Override
+	protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		
+
+		try {
+			response.setHeader("Content-Disposition", "attachment; filename=\"reporte_proveedores.xlsx\"");
+			Proveedor proveedor = (Proveedor) model.get("proveedor");
+			Sheet sheet = workbook.createSheet("Proveedor");
+			
+			CellStyle header = workbook.createCellStyle();
+			header.setBorderBottom(BorderStyle.MEDIUM);
+			header.setBorderTop(BorderStyle.MEDIUM);
+			header.setBorderRight(BorderStyle.MEDIUM);
+			header.setBorderLeft(BorderStyle.MEDIUM);
+			header.setFillForegroundColor(IndexedColors.GREEN.index);
+			header.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+			
+			Row row = sheet.createRow(0);
+			Cell cell = row.createCell(0);
+			cell.setCellValue("Datos del Proveedor");
+			cell.setCellStyle(header);
+			
+			row = sheet.createRow(1);
+			cell = row.createCell(0);
+			cell.setCellValue("Nombre");
+			cell = row.createCell(1);
+			cell.setCellValue(proveedor.getNombre());
+			
+			row = sheet.createRow(2);
+			cell = row.createCell(0);
+			cell.setCellValue("Apellidos");
+			cell = row.createCell(1);
+			cell.setCellValue(proveedor.getApellido());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+}
---
+++ desarrollo-web/desarrollo-web/src/main/java/com/desarrolloweb/spring/app/view/pdf/ProveedoresPdfView.java
@@ -0,0 +1,35 @@
+package com.desarrolloweb.spring.app.view.pdf;
+
+import java.awt.Color;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractPdfView;
+
+import com.desarrolloweb.spring.app.entities.Proveedor;
+import com.lowagie.text.Document;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+
+@Component("detalle-proveedor-form.pdf")
+public class ProveedoresPdfView extends AbstractPdfView {
+
+	@Override
+	protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		Proveedor proveedor = (Proveedor) model.get("proveedor");
+		
+		PdfPTable tabla = new PdfPTable(1);
+		tabla.setSpacingAfter(20);
+		
+		tabla.addCell(proveedor.getNombre());
+		tabla.addCell(proveedor.getApellido());
+	
+		document.add(tabla);
+	}
+
+}
