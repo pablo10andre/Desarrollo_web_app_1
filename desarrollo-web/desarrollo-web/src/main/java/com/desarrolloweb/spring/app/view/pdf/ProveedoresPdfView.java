fcbfcd525f69793443745d43dd5a110454c688e4
pablo10andre <pablo10andre@gmail.com>
2021-09-11 19:48:36 -0600

    pruebas_pdf

--- desarrollo-web/desarrollo-web/src/main/java/com/desarrolloweb/spring/app/view/pdf/ProveedoresPdfView.java
+++ desarrollo-web/desarrollo-web/src/main/java/com/desarrolloweb/spring/app/view/pdf/ProveedoresPdfView.java
@@ -1,31 +1,52 @@
 package com.desarrolloweb.spring.app.view.pdf;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
+import com.desarrolloweb.spring.app.entities.Cliente;
 import com.desarrolloweb.spring.app.entities.Proveedor;
+import com.desarrolloweb.spring.app.util.PageRender;
 import com.lowagie.text.Document;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
+import com.desarrolloweb.spring.app.repositories.ProveedorRepository;
 
-@Component("listar-proveedores.pdf")
+@Component("proveedores.pdf")
 public class ProveedoresPdfView extends AbstractPdfView {
-
+	private ProveedorRepository proveedorRepository;
 	@Override
 	protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
 			HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		Proveedor proveedor = (Proveedor) model.get("proveedor");
-		
+			  
+	
 		PdfPTable tabla = new PdfPTable(1);
 		tabla.setSpacingAfter(20);
-		tabla.addCell("hola como estas");
+		//List<Proveedor> proveedor=(List<Proveedor>) proveedorRepository.findAll();
+		//List<String> integrantes=new ArrayList<>();
+		//integrantes.add("wilson chupa pollas");
+		//integrantes.add("eddy debora penes");
+		//integrantes.add("pablo papito crack");
+		//ArrayList proveedores = new ArrayList<>();
+		//proveedores=(ArrayList) proveedorRepository.findAll();
+		
+		List<Proveedor> proveedores =  (List<Proveedor>) model.get("proveedores");
+		tabla.addCell(proveedores.get(0).getNombre());
+	
+		
+		
+		
 		document.add(tabla);
 	}
 
