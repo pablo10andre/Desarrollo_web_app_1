f314cc24a877b87a756e275a7d75d631990dad8a
Wcano74 <66225081+Wcano74@users.noreply.github.com>
2021-09-12 21:15:14 -0600

    Commit Wilson
    Diseño PDF proveedores

--- desarrollo-web/desarrollo-web/src/main/java/com/desarrolloweb/spring/app/view/pdf/ProveedoresPdfView.java
+++ desarrollo-web/desarrollo-web/src/main/java/com/desarrolloweb/spring/app/view/pdf/ProveedoresPdfView.java
@@ -15,6 +15,22 @@ import com.lowagie.text.Document;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
+import com.desarrolloweb.spring.app.entities.Proveedor;
+import com.lowagie.text.Document;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
+
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
 
 @Component("proveedores.pdf")
 public class ProveedoresPdfView extends AbstractPdfView {
@@ -28,20 +44,32 @@ public class ProveedoresPdfView extends AbstractPdfView {
 		PdfPTable tabla = new PdfPTable(1);
 		tabla.setSpacingAfter(20);
 
+	
+		//Proveedor proveedor = (Proveedor) model.get("proveedor");
+		Proveedor p = new Proveedor();
+		List<Proveedor> l = new ArrayList<Proveedor>();
+		List<Proveedor> lista = new ArrayList<Proveedor>();
+		lista.add(p);
+		PdfPCell ctitulo = new PdfPCell();
+		PdfPCell csalto = new PdfPCell();
+		Font ftitulo = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+		PdfPCell cproveedor = new PdfPCell();
+		Font fproveedor = FontFactory.getFont(FontFactory.COURIER_BOLDOBLIQUE);
+		//Color y fuente titulo
+		ftitulo.setColor(Color.CYAN);
+		ftitulo.setSize(24);
+		ctitulo.setHorizontalAlignment(ctitulo.ALIGN_CENTER);
+		ctitulo.setBorder(ctitulo.NO_BORDER);
+		ctitulo.setPhrase(new Phrase("LISTADO DE PROVEEDORES",ftitulo));
 
-		
-		
 		for(Proveedor proveedor:pr) {
 			tabla.addCell(proveedor.getNombre()+"|"+proveedor.getDireccion()+"|"+proveedor.getTelefono()+"|"+proveedor.getEmail());
 			
-			
 		}
-		
-	
-	
-		
+
 
 		document.add(tabla);
+		
 	}
 
 }
